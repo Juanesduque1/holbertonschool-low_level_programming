b1b6d5feb470fe034e95f6e9b5fc1518bc3ad64d
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-07 11:51:01 -0700

    Task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main - Main is the sixth program of c
+*Description: Prints the size of various types
+*Return: zero
+*/
+int main(void)
+{
+printf("Size of char: %ld byte(s)\n", sizeof(char);
+printf("Size of int: %ld byte(s)\n", sizeof(int);
+printf("Size of long int: %ld byte(s)\n", sizeof(long int);
+printf("Size of long long int: %ld byte(s)\n", sizeof(double);
+printf("Size of long int: %ld byte(s)\n", sizeof(long int);
+printf("Size of float: %ld byte(s)\n", sizeof(float);
+return (0);
+}
