34ca86a7a7b4c156067becc0ad4cea9957c73ad0
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-07 13:13:28 -0700

    Task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Main is the fifth program of c
+*Description: Prints the text of the exersice with printf
+*Return: zero
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
