56f6fe62d111ea46f47d0c308fcc8ad952349be9
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-07 11:39:04 -0700

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Main is the fifth program of c
+*Description: Prints the text of the exersice with printf
+*Return: zero
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
