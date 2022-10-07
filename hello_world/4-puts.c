8eba287515b50eb09a82e71d45854451d84b52c3
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-07 11:33:23 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,11 @@
-gcc -c int puts(const char* "Programming is like building a multilingual puzzle"\n)
+#include<stdio.h>
+/**
+*main - Main is the fourth program of c
+*Description: Prints a text without printf
+*Return: zero
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
