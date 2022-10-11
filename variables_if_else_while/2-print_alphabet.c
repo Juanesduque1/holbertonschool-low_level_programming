a5dc58559819469677db4cf767823a1d6dd42a63
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 17:42:21 -0700

    Task 0

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main (void) {
+   char ch;
+
+   for(ch = 'A' ; ch <= 'Z' ; ch++) {
+      putchar(ch);
+   }
+   
+   return(0);
+}
