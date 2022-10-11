3458f2d0ad975fab975ef9df0d5be83de21aba97
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 08:49:11 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Main is the first program of c
+ *Description: Prints the text of the exersice with printf
+ *Return: zero
+ */
+int main(void)
+{
+  printf("_putchar\n");
+  return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme of second project, nested loops
