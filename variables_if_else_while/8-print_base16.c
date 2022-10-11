6840b223dc1b9de2bafcb28814744ac44c40dba1
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 19:35:18 -0700

    Task 7

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
