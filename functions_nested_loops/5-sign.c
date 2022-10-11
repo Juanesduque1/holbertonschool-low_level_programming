c8b7c5365f621404432b588c3799b3776db675cc
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 14:19:47 -0700

    Task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_sign - prints the sign of a number
+*@n: variable
+*Description: Program of task 5
+*Return: 0, -1 or 1
+*/
+
+int print_sign(int n)
+{
+if (n < 0)
+printf('-');
+return (-1);
+
+if (n = 0)
+printf('0');
+return (0);
+
+if (n > 0)
+printf('+');
+return (1);
+
+}
