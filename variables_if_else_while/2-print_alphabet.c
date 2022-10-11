b4165eb1ffe5bec86bf3168c5568c723f1538b1c
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 17:44:21 -0700

    Task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
 *
 */
 
-int main (void) {
+int main (void)
+{
    char ch;
 
-   for(ch = 'A' ; ch <= 'Z' ; ch++) {
-      putchar(ch);
-   }
-   
+   for(ch = 'a' ; ch <= 'z' ; ch++)
+   putchar(ch);
+
    return(0);
 }
