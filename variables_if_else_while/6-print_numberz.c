de570c4cccf4454f12ef1a87e81da256974088a1
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 19:24:11 -0700

    Task 5

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+int putchar(int ch);
+
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
