7a7977f0345a9a8722b66492cd2b6aacb100501c
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 19:05:07 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
