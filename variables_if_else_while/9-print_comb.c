313a953c1016cd032c54ccbf89c1e2f6be20ded6
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 19:39:19 -0700

    Task 7

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+int ch;
+
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+putchar(', ');
+}
+putchar('\n');
+
+return (0);
+}
