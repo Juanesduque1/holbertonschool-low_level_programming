00f82da8abee6e9f9d0e6d0b187996fd4e0b95b4
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-10 19:29:13 -0700

    Task 5

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'z' ; ch <= 'a' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
