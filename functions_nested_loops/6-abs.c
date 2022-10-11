a2a9a3ab98dd062dc04c4ce77686de3e540652b3
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 14:55:25 -0700

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - Check if string is lowercase
+*Description: Program of task 6
+*Return: 0
+*/
+
+int _abs()
+{
+abs();
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,5 @@ void print_alphabet_x10(void);
 int _islower(int ch);
 int _isalpha(int ch);
 int print_sign(int n);
+int _abs(int);
 #endif
