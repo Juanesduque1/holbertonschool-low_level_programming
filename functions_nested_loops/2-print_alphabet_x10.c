deb4c46a0563c457f085143c401a9757a06f0c44
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 12:27:37 -0700

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* print_alphabet - print alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+int  i = '1';
+for (i <=10)
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+i++;
+}
