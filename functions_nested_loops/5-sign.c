bdc021493694b50745778f3fe6c6cf930cb5768a
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 14:41:11 -0700

    Task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,16 +14,18 @@ int print_sign(int n)
 if (n < 0)
 {
 printf("-");
-}
 return (-1);
-
-if (n == 0)
+}
+ 
+else if (n == 0)
 {
 printf("0");
 return (0);
 }
 
-if (n > 0)
+else
+{
 printf("+");
 return (+1);
+}
 }
