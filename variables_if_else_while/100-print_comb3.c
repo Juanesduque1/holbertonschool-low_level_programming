64d720534204e603a923104fbd9296cc304a5185
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 09:22:58 -0700

    Task 100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Zero
+*
+*/
+
+int main(void)
+{
+char ch1;
+char ch2;
+
+for (ch1 = '0' ; ch1 <= '9' ; ch1++)
+for (ch2 = '0' ; ch2 <= '9' ; ch2++)
+{
+{
+if (ch1 != ch2)
+putchar(ch1);
+putchar(ch2);
+}
+}
+
+{
+putchar('\n');
+
+return (0);
+}
