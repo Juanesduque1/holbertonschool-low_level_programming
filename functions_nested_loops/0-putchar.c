265c569992764942b4e4b979a37c0d71ea62710a
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 08:57:15 -0700

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,15 +8,8 @@
  */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+
+printf("_putchar\n");
 
 return (0);
 
