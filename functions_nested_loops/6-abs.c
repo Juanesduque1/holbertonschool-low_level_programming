b6672b3a4bac53725330b078cbc33bceb5cf95ad
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-11 15:08:29 -0700

    Task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,16 +4,17 @@
 #include "main.h"
 /**
 *_abs - Check if string is lowercase
+*@n: variable
 *Description: Program of task 6
 *Return: 0
 */
 
-int _abs()
+int _abs(int n)
 {
 if (n < 0)
 n = n*-1;
 return (n);
 
 else
-return (n)
+return (n);
 }
