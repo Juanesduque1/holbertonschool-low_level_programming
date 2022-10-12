2fdbdd36912b27013b3039c1ebbf6d6e3414afb3
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-12 16:20:32 -0500

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,38 +10,38 @@
 
 void times_table(void)
 {
-    int l;
-    int c;
-    int i;
+int l;
+int c;
+int i;
 
-    i = 0;
+i = 0;
 
-    for (l = 0; l <= 9; l++)
-    {
-    for (c = 0; c <= 9; c++)
-    {
-    int r;
-    r = i * c;
-    if (r > 9)
-    {
-    _putchar(',');
-    _putchar(' ');
-    _putchar((r / 10) + '0');
-    _putchar((r % 10) + '0');
-    }
-    else if (c == 0)
-    {
-    _putchar(r + '0');
-    }
-    else if (r <= 9)
-    {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(' ');
-    _putchar(r + '0');
-    }
-    }
-    i++;
-    _putchar('\n');
-    }
+for (l = 0; l <= 9; l++)
+{
+for (c = 0; c <= 9; c++)
+{
+int r;
+r = i * c;
+if (r > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+}
+else if (c == 0)
+{
+_putchar(r + '0');
+}
+else if (r <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(r + '0');
+}
+}
+i++;
+_putchar('\n');
+}
 }
