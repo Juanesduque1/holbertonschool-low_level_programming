fe3d4c6f699015a356f5750b46010952df75a996
Juan Esteban <juanesduquemorales@gmail.com>
2022-10-12 09:23:13 -0700

    Task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*jack_bauer - Check if string is lowercase
+*@h: variable of hours
+*@m: vairable of minutes
+*Description: Program of task 8
+*Return: last digit
+*/
+
+void jack_bauer(void)
+{
+for (int h == 0; h <=23; h++);
+for (int i == 0; i <=59; i++);
+_printchar((h / 10) + '0');
+_printchar((h % 10) + '0');
+_printchar(':');
+_printchar((i / 10) + '0');
+_printchar((i % 10) + '0');
+_printchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int _isalpha(int ch);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int n);
+void jack_bauer(void);
 #endif
