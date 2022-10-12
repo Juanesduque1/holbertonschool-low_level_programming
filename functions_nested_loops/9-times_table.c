54a4822d964859426a3f4fe91ec3a16939b38d8b
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-12 12:58:48 -0500

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*jack_bauer - Check if string is lowercase
+*Description: Program of task 8
+*Return: last digit
+*/
