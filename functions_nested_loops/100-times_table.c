b65dfb4704f4b401f5e9c759c6aa52c4022ac370
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-13 10:26:18 -0500

    Task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,47 +7,38 @@ void print_times_table(int n)
 {
 int l, c, i;
 i = 0;
-for (l = 0; l <= n; l++)
-{
-if (n <= 15)
-{
-for (c = 0; c <= n; c++)
-{
+for (l = 0; l <= n; l++){
+if (n <= 15){
+for (c = 0; c <= n; c++){
 int r;
 r = i *c;
-if (r > 9 && r < 100)
-{
+if (r > 9 && r < 100){
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((r / 10) + '0');
 _putchar((r % 10) + '0');
 }
-else if (c == 0)
-{
+else if (c == 0){
 _putchar(r + '0');
 }
-else if (r <= 9)
-{
+else if (r <= 9){
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(r + '0');
 }
-else if (r >= 100)
-{
+else if (r >= 100){
 _putchar(',');
 _putchar(' ');
 _putchar(((r / 10) / 10) + '0');
 _putchar(((r / 10) % 10) + '0');
 _putchar((r % 10) + '0');
-}
-}
+}}
 i++;
 _putchar('\n');
 }
-else
-{
+else{
 break;
 }}}
