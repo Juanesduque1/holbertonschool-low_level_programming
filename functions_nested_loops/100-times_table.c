615967bceb787a7c1a89127c43f2dab8107b4446
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-13 10:37:09 -0500

    Task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -24,11 +24,13 @@ _putchar((r % 10) + '0');
 else if (c == 0)
 _putchar(r + '0');
 else if (r <= 9)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(r + '0');
+}
 else if (r >= 100)
 {
 _putchar(',');
@@ -41,6 +43,5 @@ i++;
 _putchar('\n');
 }
 else
-{
 break;
-}}}
+}}
