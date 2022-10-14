eec85de6b4b9f759daf8a4813ba1d82bc83ec726
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-14 11:22:30 -0500

    Task 6

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_diagonal - Print diagonal n
+*@n: Variable of how many times the program has to print '\'
+*Description: Program of task 7
+*Return: Diagonal
+*/
+
+void print_diagonal(int n)
+{
+int c;
+
+for (c = 1; c <= n; c++)
+{
+_putchar('\\');
+}
+_putchar('\n');
+}
