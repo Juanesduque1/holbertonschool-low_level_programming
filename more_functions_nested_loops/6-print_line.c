1b4b1330a87feb70e19c26c9a85cbb58bbd451f0
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-14 11:18:45 -0500

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_line - Print line n
+*Description: Program of task 6
+*Return: Line
+*/
+
+void print_line(int n);
+{
+int c;
+
+for (c = 0; c <= n; c++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
