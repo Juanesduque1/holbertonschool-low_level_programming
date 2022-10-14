3ee5437e17ef780cdf700c814993e6d1b7ecd4cd
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-13 23:25:33 -0500

    Task 3

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_most_numbers - Print numbers from 0 to 9 except 2 and 4
+*Description: Program of task 4
+*Return: 01356789
+*/
+
+void print_most_numbers(void)
+{
+int i = 0;
+
+while (i <= 9)
+{
+if ((i != 2) || (i != 4))
+{
+putchar(i + '0');
+}
+i++;
+}
+putchar('\n');
+}
