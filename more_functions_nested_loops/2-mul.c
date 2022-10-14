1f2d1dcf01a7c6df48fe4fbe1f4c71b312746d6f
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-13 23:16:07 -0500

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*_isdigit - Check if variable is digit
+*@c: variable
+*Description: Program of task 1
+*Return: 0 or 1
+*/
+
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+printf(mul);
+}
