cf8d4557987185e12036ce8dabf1220892853dc2
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-13 23:33:21 -0500

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,18 @@ void more_numbers(void)
 int i = 0;
 int c = 0;
 
-for ( c = 0; c <= 10; c++)
+for (c = 0; c <= 10; c++)
 {
 while (i <= 14)
 {
+if (i < 10)
+{
 putchar(i + '0');
 i++;
+}
+else
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 }
 putchar('\n');
 }
