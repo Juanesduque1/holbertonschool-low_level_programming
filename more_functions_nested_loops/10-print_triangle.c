57235ccfa82c1e419aec679a51b192efdce66d79
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-14 14:46:06 -0500

    Task 9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_triangle - Print triangle n
+*@size: Variable of how many times the program has to print '#'
+*Description: Program of task 10
+*Return: Triangle
+*/
+
+void print_triangle(int size)
+{
+int i;
+int c;
+
+for (i = 0; i < size; i++)
+{
+_putchar(' ');
+}
+for (c = size; c > i; size--)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
