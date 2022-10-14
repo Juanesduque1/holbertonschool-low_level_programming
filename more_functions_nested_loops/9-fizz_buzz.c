63b17a5089aade8428451b02544b46158501c34c
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-14 14:16:11 -0500

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int i;
 
 for (i = 0; i <= 100; i++)
 {
-if ((i % 3) == 0 && (i % 5) == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if ((i % 5) == 0)
+else if (i % 5 == 0)
 {
 printf("Buzz");
 }
-else if ((i % 3) == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
