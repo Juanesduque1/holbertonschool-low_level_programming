8047b296ee95f8ca104f1633714988bf39649ee3
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-17 19:20:07 -0500

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*swap_int - Swap the value of two variables
+*@a: Variable one
+*@b: Variable two
+*
+*Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+int c;
+c = 0;
+
+while (c <= *s)
+{
+    c++;
+}
+}
