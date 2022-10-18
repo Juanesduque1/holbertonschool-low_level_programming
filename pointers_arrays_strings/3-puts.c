d017a5de2b8f8be40526c7dd047d7221da7f80ff
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-17 19:38:13 -0500

    Task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - returns a string
+*@str: Variable
+*
+*Return: Always 0
+*/
+
+void _puts(char *str)
+{
+int c;
+c = 0;
+
+while (str[c] != '\0')
+{
+_putchar(c)
+}
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
