44ac1e1443013b191e5b5c54f8e8f09715a12e3f
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-18 18:52:26 -0500

    Second project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+/* FROM PROJECT 1*/
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,4 +12,13 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+/* FROM PROJECT 2*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 #endif
