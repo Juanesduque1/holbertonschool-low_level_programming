0b1e7a5a77183f2d3c31f298570b18fe05d05108
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-18 17:11:55 -0500

    Task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*puts2 - returns a string reversed
+*@str: Variable
+*
+*Return: Always 0
+*/
+
+void puts2(char *str)
+{
+int len;
+int i;
+for (len = 0; str[len] != '\0'; len++)
+{}
+
+for (i = 0; i < len; i+=2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
