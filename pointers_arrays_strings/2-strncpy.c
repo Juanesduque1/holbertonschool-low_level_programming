c203ed3ad6dfe1d87d930e6ff0166cd6ca33af00
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-19 16:12:35 -0500

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,13 +15,13 @@ int i = 0;
 
 while (src[i] != '\0' && i < n)
 {
-    dest[i] = src[i];
-    i++;
+dest[i] = src[i];
+i++;
 }
 while (i < n)
 {
-    dest[i] = '\0';
-    i++;
+dest[i] = '\0';
+i++;
 }
 return (dest);
 }
