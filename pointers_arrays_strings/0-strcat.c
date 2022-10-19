c9eeebce82cbb26f178058cf746efa67a56ca015
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-19 09:16:38 -0500

    Task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**_strcat - Concatenate two strings
+*@dest: Variable of the destination
+*@src: Variable of the string
+*
+*Return: Always 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+    char i = dest;
+
+    while(dest != '\0')
+    {
+        dest++;
+    }
+
+    while(*src != '\0')
+    {
+        dest =src;
+        dest++;
+        src++;
+    }
+
+/**dest = '\0';*/
+/*return i;*/
+}
