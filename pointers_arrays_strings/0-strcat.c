fcdc129fd781340591cb9659ec85cae25321b385
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-19 09:18:51 -0500

    Task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-    char i = dest;
+char i = dest;
 
-    while(dest != '\0')
-    {
-        dest++;
-    }
-
-    while(*src != '\0')
-    {
-        dest =src;
-        dest++;
-        src++;
-    }
-
-/**dest = '\0';*/
-/*return i;*/
+while(*dest != '\0')
+{
+dest++;
+}
+while(*src != '\0')
+{
+dest = src;
+dest++;
+src++;
+}
+*dest = '\0';
+return i;
 }
