1f87ed8f6bfa63e6abf274c49590037425b211b3
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 11:17:19 -0500

    Task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 {
 s1++;
 s2++;
 }
-
-if(*s1 == *s2)
+if (*s1 == *s2)
 {
-return 0;
+return (0);
 }
-
 else
-{
-return *s1 - *s2;
+return (*s1 - *s2);
 }
-}
-
