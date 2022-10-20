23e54bd4698dad64e263c8bdc57ed10363c24ea0
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 14:45:36 -0500

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**string_toupper - Returns the string in uppercase
+*@c: Variable
+*
+*Return: Always 0
+*/
+
+char *string_toupper(char *c)
+{
+char c;
+int i;
+i = 0;
+
+while (c[i])
+{
+_putchar(toupper(c[i]));
+i++;
+}
+}
