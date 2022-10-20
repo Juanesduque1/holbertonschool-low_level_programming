70fd2cce38aaf4c87894bcd9254ce16be709ea6b
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 15:19:08 -0500

    Task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*puts2 - returns a string from with i += 2
+*@str: Variable
+*
+*Return: Always 0
+*/
+
+char *leet(char *c)
+{
+
+int i;
+int n;
+n = 10;
+char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+
+for (i = 0; i <= n; i++)
+{
+    if (i <= n)
+    {
+        a[i] = b[i]
+    }
+}
+return (a);
+}
