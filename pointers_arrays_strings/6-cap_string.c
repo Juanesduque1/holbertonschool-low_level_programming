435466883966c8aa0f8d2cbd60f916711eacc295
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 17:27:12 -0500

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**cap_string - Returns the string in uppercase
+*@c: Variable
+*
+*Return: Always 0
+*/
+
+char *cap_string(char *c)
+{
+
+int i, j;
+char array[] = " \t\n,;.!?\"(){}";
+
+i = 0;
+
+if (c[0] >= 97 && c[0] <=122)
+{
+c[0] -= 32;
+}
+while (c[i] != '\0')
+{
+if (c[i + 1] >= 97 && c[i] <= 122)
+{
+for (j = 0; array[j] != '\0'; j++)
+{
+if (array[j] == c[i])
+{
+c[i +1] -= 32;
+break;
+}
+}
+}
+i++;
+}
+return (c);
+}
