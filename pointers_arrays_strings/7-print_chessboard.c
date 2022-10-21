644100d0220b9ae98063ca20e457615a9df22c39
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-21 14:49:17 -0500

    Task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_chessboard - Print the chessboard saved in a
+*@a: Chessboard
+*
+*Return: Always 0
+*/
+
+void print_chessboard(char (*a)[8])
+{
+
+int i;
+int j;
+
+for (i = 0; i <= 8; i++)
+{
+for (j = 0; j <= 8; j++)
+{
+_putchar(a[i][j]);
+_putchar('\n');
+}
+}
+}
