3d16d72b112320b7b55c30af9c982a7c53272b72
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 23:00:14 -0500

    Task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+**_strchr - Return the memcpy function
+*@s: String
+*@c: Character to find
+*
+*Return: Always 0
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+
+
+}
