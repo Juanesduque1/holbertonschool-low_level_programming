1ae4dd20679886f73cceaff8110e3e37891f5c9d
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-20 23:08:05 -0500

    Task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-**_strchr - Return the memcpy function
+**_strspn - Return the quantity of characters contained in a string
 *@s: String
-*@c: Character to find
+*@accept: Substring
 *
 *Return: Always 0
 */
@@ -12,6 +12,21 @@
 unsigned int _strspn(char *s, char *accept)
 {
 
+unsigned int i;
+unsigned int j;
+unsigned int c;
 
+c = 0;
 
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+c++;
+}
+}
+}
+return (c);
 }
