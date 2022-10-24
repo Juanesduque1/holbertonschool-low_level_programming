043a9b0000522b3a34a06b9e7d188f16722dc0bd
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-24 16:39:01 -0500

    Task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -11,16 +11,13 @@
 int _strlen_recursion(char *s)
 {
 
-	int i = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		_strlen_recursion(++s);
+		return (0);
 	}
 	else
 	{
-		return (i);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
