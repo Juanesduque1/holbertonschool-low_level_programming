9de91b1e95b05b9cbdd00d6e70a50f83103119a3
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-24 17:28:52 -0500

    Task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*factorial - Function that prints the factorial of an integer
+*@n: Integer
+*
+*Return: Always 0
+*/
+
+int factorial(int n)
+{
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n -1));
+	}
+	return (0);
+}
