f5c6da4f10983026efdbd0423c1bc429c1b833d5
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-24 18:58:48 -0500

    Task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_sqrt_recursion - Return square root of n
+*@n: Integer
+*
+*Return: Always 0
+*/
+
+int _sqrt_recursion(int n)
+{
+	square_root_wrapper(n, 1);
+}
+
+/**
+*_sqrt_recursion - Return square root of n with other parameter for the guess
+*@n: Integer
+*@guess: Integer
+*
+*Return: Always 0
+*/
+
+int square_root_wrapper(int n, int guess)
+{
+
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+
+	else if((guess < 0) || (guess > n))
+	{
+		return (-1);
+	}
+
+	else 
+	{
+		return square_root_wrapper(n, guess + 1);
+	}
+}
