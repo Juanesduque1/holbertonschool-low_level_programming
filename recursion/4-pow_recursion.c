1cf80a877cea1e6005ccd53392afea1f585456d9
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-24 17:38:19 -0500

    Task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_pow_recursion - Function that prints the pow of two integers
+*@x: Integer
+*@y: Integer 2
+*
+*Return: Always 0
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (factorial(x - 1) ^ y);
+	}
+}
