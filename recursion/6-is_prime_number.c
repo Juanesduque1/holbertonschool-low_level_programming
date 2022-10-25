ed1f5e5d5d565535b29a423281ef4c63cd148546
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-24 19:13:08 -0500

    Task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*check_prime - Return if a number is prime with 2 parameters
+*@n: Number
+*@i: Parameter
+*
+*Return: Always 0
+*/
+
+int check_prime(int n, int i)
+{
+
+	if (n == i)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (check_prime(n, i + 1));
+	}
+}
+
+/**
+*is_prime_number - Return if a number is prime
+*@n: Number
+*
+*Return: Always 0
+*/
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
