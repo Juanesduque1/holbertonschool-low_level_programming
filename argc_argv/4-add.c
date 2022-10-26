94a8717a78fd88916402958d321b74349446ce26
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-26 16:33:01 -0500

    Task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != 0; j++)
 		{
-		if ((atoi(argv[i][j]) < '0') || atoi(argv[i][j] > '9'))
+		if ((argv[i][j] < '0') || argv[i][j] > '9')
 		{
 			printf("Error\n");
 			return (1);
