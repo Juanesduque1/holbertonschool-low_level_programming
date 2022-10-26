4392405e16ce4987d801b39540a462ec5dac7ad8
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-26 12:45:22 -0500

    Task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,18 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int count;
+	int null = 0;
 
 	count = argc - 1;
 
 	if (argv != 0)
 	{
 	if (count == 0)
-		return (0);
+		printf("%d\n", null);
 
 	if (count >= 1)
-	{
 		printf("%d\n", count);
-	}
 	}
 	return (0);
 }
