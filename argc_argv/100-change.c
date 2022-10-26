e07cfcf2195e5b3897a62cd6c6f7aab0ba83296e
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-26 17:20:53 -0500

    Task 5

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -12,22 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-		printf("Error\n");
-		return (1);
-
-	int i, j;
-	int total = atoi(argv[1]);
+	int total, i, j;
 	char coins[] = {25, 10, 5, 2, 1};
 
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int total = atoi(argv[1]);
+
 	if (total < 0)
+	{
 		printf("0");
 		return (0);
+	}
 
 	for (i = 0; coins[i] != '\0'; i++)
 	{
 		if (coins[i] > total)
+		{
 			continue;
+		}
 
 		for (j = 0; coins[i] >= total; j++)
 		{
