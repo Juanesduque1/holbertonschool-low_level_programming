289584f2af703b2a5264486c0262f14721c1af34
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-31 18:46:33 -0500

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,17 +14,21 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	char *array;
+	int **array;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
+
 	array = (int **)malloc(height*sizeof(int*)); 
 
 	for (i = 0; i < height; i++) 
 		array[i] = (int*)malloc(width*sizeof(int)); 
+
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
@@ -34,4 +38,54 @@ int **alloc_grid(int width, int height)
 		}
 	}
 	return (array);
+}
+
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
+{
+    int w;
+    int h;
+
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int **grid;
+
+    grid = alloc_grid(6, 4);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 6, 4);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 6, 4);
+    return (0);
 }
