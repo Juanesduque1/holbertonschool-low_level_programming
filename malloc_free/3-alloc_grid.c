d539fe842044f3255c03286b13f26846bd403e95
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-31 18:39:12 -0500

    Task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+**alloc_grid - Returns a 2d array of integers
+*@width: Width
+*@height: Height
+*
+*Return: Array result or NULL
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int n, i, j;
+	char *array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	array = (int **)malloc(height*sizeof(int*)); 
+
+	for (i = 0; i < height; i++) 
+		array[i] = (int*)malloc(width*sizeof(int)); 
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
