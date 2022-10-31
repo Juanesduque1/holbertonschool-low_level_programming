67c60e1b17627a3bb6a3138b50d4afe4ba820685
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-31 14:18:59 -0500

    Task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
 /**
 **create_array - Return a array of a single character using Malloc
 *@size: Size of the array
-*@i: Character to return 'size' times in the array
+*@c: Character to return 'size' times in the array
 *
 *Return: Array
 */
@@ -14,8 +14,8 @@ char *create_array(unsigned int size, char c)
 {
 
 	unsigned int i = 0;
-    char *array;
-	array = malloc(size * sizeof(c));
+	char *array;
+	array = malloc(size);
 
 	if (size == 0)
 	{
