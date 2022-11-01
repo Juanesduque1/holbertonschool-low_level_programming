a4db2cd1c4f9c81f1cf020a12dc7aa180711bdd9
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 18:01:21 -0500

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,8 +5,9 @@
 #include <string.h>
 
 /**
-**malloc_checked - Allocates the memory with malloc
-*@b: Size to allocate
+**string_nconcat - Concatenate two strings
+*@s1: String 1
+*@s2: String 2
 *
 *Return: Always 0
 */
@@ -20,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-	
+
 	if (s2 == NULL)
 		s2 = "";
 
@@ -37,10 +38,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < len1; i++)
 		array[i] = s1[i];
-	
+
 	for (j = 0; j < n && j < len2; i++, j++)
 		array[i] = s2[j];
-	
-	array[i] = '\0';
+
 	return (array);
 }
