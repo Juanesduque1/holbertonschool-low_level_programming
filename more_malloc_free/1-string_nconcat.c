d93a7cfad8b68ed8f9593f8f8e686877673060f5
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 16:55:42 -0500

    Task 3

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+**malloc_checked - Allocates the memory with malloc
+*@b: Size to allocate
+*
+*Return: Always 0
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	
+
+}
---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+**array_range - Creates an array of integers
+*@min: Minimum value
+*@max: Maximum value
+*
+*Return: Always 0
+*/
+
+int *array_range(int min, int max)
+{
+	int i, j = 0;
+	char *array;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(max);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++, j++)
+		array[j] = i;
+
+	return (array);
+}
