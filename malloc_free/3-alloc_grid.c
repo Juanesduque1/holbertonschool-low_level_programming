c6a45f4cdd5766ccfc981b80a9e7c0dcdc84c1c8
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 17:39:18 -0500

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	array = (int **)malloc(height * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 
 	if (array == NULL)
 	{
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int *)malloc(width * sizeof(int));
+		array[i] = malloc(width * sizeof(int));
 
 		if (array[i] == NULL)
 		{
