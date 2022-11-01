144d1ef45887acca9ce486779f5bec990748da1c
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 14:19:43 -0500

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+**_strdup - Return a copy of a string
+*@str: Original string
+*
+*Return: Copy of the string
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	char *ar = malloc(b);
+
+	return (ar);
+}
