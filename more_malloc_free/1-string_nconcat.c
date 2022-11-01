30cafa4187b17ed819b8570a8e45d75dad3c47c5
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 17:55:56 -0500

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	
+	int i, j;
+	int len1, len2;
+	char *array;
 
+	if (s1 == NULL)
+		s1 = "";
+	
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1) + 1;
+	len2 = strlen(s2) + 1;
+
+	if (n > len2)
+		array = malloc(len1 + len2 + 1);
+	else
+		array = malloc(len1 + n + 1);
+
+	if (array == 0)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		array[i] = s1[i];
+	
+	for (j = 0; j < n && j < len2; i++, j++)
+		array[i] = s2[j];
+	
+	array[i] = '\0';
+	return (array);
 }
