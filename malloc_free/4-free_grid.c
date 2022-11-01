c177e39a2e9b6e0053272fba7a5e820e29ffce41
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-10-31 19:55:44 -0500

    Task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*free_grid - Frees the grid array
+*@grid: 2D array of integers
+*@height: Height
+*
+*Return: Array result or NULL
+*/
+
+void free_grid(int **grid, int height)
+{
+
+	int i = 0;
+	int width;
+	int j;
+
+	while (grid[i] != '\0')
+	{
+		i++;
+		width++;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			free(grid[i][j]);
+		}
+	}
+	free(grid);
+}
