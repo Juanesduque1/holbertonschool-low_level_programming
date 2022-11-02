e14366b595c9f2ff97979fff02e8fcaa2bb33b47
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-01 23:48:45 -0500

    Task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+**main - Print file name
+*@argc: argc
+*@argv: argv
+*
+*Return: Always 0
+*/
+
+int main(int argc, char ** argv)
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
