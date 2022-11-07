4145798abb181d36df8b1a2481b831e3e459b5d2
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 16:15:18 -0500

    Task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,6 +3,11 @@
 
 /**
  * struct dog: Struct to declare dog
+ * @name: Name of the dog
+ * @owner: Owner name
+ * @age: Age of the dog
+ * 
+ * Description: Structures and typedef project
  */
 
 typedef struct dog
