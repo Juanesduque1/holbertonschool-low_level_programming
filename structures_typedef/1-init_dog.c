54043de748d559f97da60ed49fb73d8a630c1dbb
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 19:27:14 -0500

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,26 +1,23 @@
 #include "dog.h"
-#include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * init_dog: Initialize a variable with struct
+ * init_dog - Initialize a variable with struct
  * @d: Struct in dog.h
  * @name: Name of the dog
  * @owner: Owner name
  * @age: Age of the dog
  *
- * Description - Structures and typedef project
+ * Return: Always 0
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d)
 	{
-		printf("Ok\n");
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
 	}
-
-	d->name = name;
-	d->owner = owner;
-	d->age = age;
-
 }
