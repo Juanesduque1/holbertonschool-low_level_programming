09292f217767d5bb4b3d583bc7c19370898f80bf
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-08 10:40:08 -0500

    Task 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,10 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
- * print_dog - Prints the structure of dog
- * @d: Struct in dog.h
+ * new_dog - Returns a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
  * Return: Always 0
  */
@@ -12,7 +15,50 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	struct name = namep;
-	
+	dog_t *newDog;
+	int lenowner, lenname, i, j;
+
+
+	newDog = malloc(sizeof(dog_t));
+	lenname = strlen(name);
+	lenowner = strlen(owner);
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		newDog->name = name;
+		newDog->age = age;
+		newDog->owner = owner;
+	}
+
+	newDog->name = malloc(lenname + 1);
+	if (newDog->name == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+
+	for (i = 0; i < lenname; i++)
+	{
+		newDog->name[i] = name[i];
+	}
+
+	newDog->owner = malloc(lenowner + 1);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	for (j = 0; j < lenowner; j++)
+	{
+		newDog->owner[j] = owner[j];
+	}
+	return (newDog);
 
 }
