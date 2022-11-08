8b3f3677c4a440f288802bd1a9f333f6308e7a5e
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 19:10:46 -0500

    Task 0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog dog - Initialize a variable with struct
+ * @name: Name of the dog
+ * @owner: Owner name
+ * @age: Age of the dog
+ *
+ * Description - Structures and typedef project
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog;
+
+	dog.name = name;
+	dog.owner = owner;
+	dog.age = age;
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,4 +16,6 @@ typedef struct dog
 	char *owner;
 	float age;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
