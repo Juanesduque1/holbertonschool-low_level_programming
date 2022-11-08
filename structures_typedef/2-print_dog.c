12dfb18ec9b899f938a5182a3a2ddc2e014a18e7
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 20:04:48 -0500

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,6 +20,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
+	if (d->age)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
 	if (d->owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
