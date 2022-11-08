1c596ff624601e7944398a264c27c14e14759e87
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 19:21:06 -0500

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,8 @@
 #include "dog.h"
 
 /**
- * init_dog dog - Initialize a variable with struct
+ * init_dog dog: Initialize a variable with struct
+ * @d: Struct in dog.h
  * @name: Name of the dog
  * @owner: Owner name
  * @age: Age of the dog
@@ -11,6 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	struct dog *d;
 
 	d->name = name;
 	d->owner = owner;
