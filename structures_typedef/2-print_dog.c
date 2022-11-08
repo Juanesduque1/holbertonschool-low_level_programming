902c0049554de28901643c819e3498803867ea4b
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-07 19:46:55 -0500

    Task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Prints the structure of dog
+ * @d: Struct in dog.h
+ * @name: Name of the dog
+ * @owner: Owner name
+ * @age: Age of the dog
+ *
+ * Return: Always 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("");
+	}
+	else
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)");
+		}
+		if (d->age != 0)
+		{
+			printf("Age: %i", d->age);
+		}
+		else
+		{
+			printf("Age: (nil)");
+		}
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)");
+		}
+	}
+}
