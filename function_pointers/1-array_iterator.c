52362b87bb5a2de399a459aec948bb2f478dbf79
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-08 19:05:04 -0500

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL)
+	if (array != NULL || action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
