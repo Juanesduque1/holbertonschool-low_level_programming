0e85ec19bc2c3cc7dfb453ccc517e85d4f59ae47
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-10 16:45:21 -0500

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -29,14 +29,3 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_end(ptr);
 	}
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
-}
