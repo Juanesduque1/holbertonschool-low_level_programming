01ad1f91ed94c1ba8468ba02afd6043b0baf1799
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-10 16:48:22 -0500

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,9 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		for (i = 0; i < n; i++)
 		{
-			if (i < n)
+			if (i < n - 1)
 				printf("%d%s", va_arg(ptr, int), separator);
-			if (i == n)
+			if (i == n - 1)
 				printf("%d", va_arg(ptr, int));
 		}
 		printf("\n");
