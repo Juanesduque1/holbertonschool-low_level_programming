c25b2bdb239e0de4736b6d65698d322c6feae0bd
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-09 19:36:07 -0500

    Task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -12,24 +12,24 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
 	while (ops[i].op)
 	{
-        if (*s == *ops[i].op && strlen(s) == 1)
-        {
-		    return (ops[i].f);
-        }
-        i++;
+		if (*s == *ops[i].op && strlen(s) == 1)
+		{
+			return (ops[i].f);
+		}
+		i++;
 	}
 
-    return (NULL);
+	return (NULL);
 }
