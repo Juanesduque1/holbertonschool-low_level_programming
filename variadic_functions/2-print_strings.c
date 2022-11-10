9e4c38cf555a6817914738d574e130a2f0d90061
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-10 18:00:51 -0500

    Task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,22 +14,25 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char * pointres;
 
 	va_list(ptr);
 	va_start(ptr, n);
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
+		pointres = va_arg(ptr, char *);
 
-		if (va_arg(ptr, char *) != NULL)
-			printf("%s", va_arg(ptr, char *));
+		if (pointres != NULL)
+			printf("%s", pointres);
 		else
 			printf("(nil)");
 
 		if (i < n - 1 && separator != NULL)
-			printf("%s%s", va_arg(ptr, char *), separator);
-
+			printf("%s%s", pointres, separator);
+	i++;
 	}
+
 	printf("\n");
 	va_end(ptr);
 }
