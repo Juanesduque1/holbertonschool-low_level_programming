d4acf0b5c73b74c3afbdfc8929d3ec2d302400db
Juan Esteban Duque Morales <juanesduque@MacBook-Air-de-Juanes.local>
2022-11-11 14:51:44 -0500

    Task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void type_char(const char *separator, char *s)
+{
+	printf("%s%s", separator, s);
+}
+
+void type_int(const char *separator, int n)
+{
+	printf("%s%i", separator, n);
+}
+
+void type_float(const char *separator, float n)
+{
+	printf("%s%d", separator, n);
+}
+
+void type_pointer(const char *separator, char *p)
+{
+	printf("%s%s", separator, p);
+}
+
+/**
+*print_all - Prints anything
+*@format: Type of data
+*
+*Return: Zero
+*
+*/
+
+void print_all(const char * const format, ...)
+{
+
+	int i = 0;
+	char *separator = ", ";
+	va_list(formats);
+
+	type_t types[] = {
+		{"c", type_char},
+		{"i", type_int},
+		{"f", type_float},
+		{"s", type_pointer}
+	};
+
+	while (types[i].t)
+	{
+		if (types[i].t == format)
+			types[i].f();
+		i++;
+	}
+
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+/**
+ * struct type - Struct type
+ *
+ * @type: Type of the argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *t;
+	void (*f)();
+} type_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
