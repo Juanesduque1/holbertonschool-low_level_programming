f205969a3e2cdb0ec74328184eb180f1343e655a
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-01 17:21:23 -0500

    Task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (!head)
+	if (!head || index > strlen(head))
 		return (NULL);
 
 	for (i = 0; i < index; i++)
