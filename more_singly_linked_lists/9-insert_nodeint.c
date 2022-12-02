f14af8205be9b6ad667658e83db8a5bf18ed134c
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-02 11:29:27 -0500

    Task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,18 +22,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 
+	aux = *head;
+	len_aux = *head;
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
-	aux = *head;
-	len_aux = *head;
-	new_node->n = n;
-	new_node->next = NULL;
-
 	while (len_aux)
 	{
 		len_aux = len_aux->next;
