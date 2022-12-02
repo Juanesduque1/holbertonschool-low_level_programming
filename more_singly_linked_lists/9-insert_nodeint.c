b62d505119a2f1362c6cc438cca458e0c6a53af9
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-02 11:22:36 -0500

    Task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
-	listint_t *aux, *new_node = NULL;
-	unsigned int i = 0;
+	listint_t *len_aux, *aux, *new_node = NULL;
+	unsigned int i = 0, len = 0;
 
 	if (!head || !*head)
 		return (NULL);
@@ -21,28 +21,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	
+
 	aux = *head;
+	len_aux = *head;
 	new_node->n = n;
 	new_node->next = NULL;
 
+	while (len_aux)
+	{
+		len_aux = len_aux->next;
+		len++;
+	}
+	if (idx > len)
+		return (NULL);
 	if (idx == 0)
-    {
-		new_node->next = aux;
+	{
+		new_node->next = *head;
 		*head = new_node;
-		return (*head);
-    }
-	if (!idx)
 		return (new_node);
-
+	}
 	while (i < idx - 1)
 	{
 		aux = aux->next;
 		i++;
 	}
-
 	new_node->next = aux->next;
 	aux->next = new_node;
-
 	return (aux);
 }
