e0cc85ae285263a66e4748a9ec2d4fc54d719a45
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-04 22:43:57 -0500

    Task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -3,14 +3,17 @@
 /**
 *clear_bit - Sets the value of a bit to 0 at given index
 **@n: Bit
-*@index: Index 
+*@index: Index
 *
 *Return: Always 0
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
 
+	*n &= ~(1 << index);
 
-
+	return (1);
 }
