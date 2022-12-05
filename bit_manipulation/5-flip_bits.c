10d879988fcc160dde19cb9c1d54ddacc6c17fd0
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-04 22:50:43 -0500

    Task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,15 +2,23 @@
 
 /**
 *flip_bits - Flip bits from one number to another
-*@n: First bit
-*@m: Second bit
+*@n: First variable
+*@m: Second variable
 *
 *Return: Always 0
 */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int flip = 0, num = n ^ m;
 
+	if (n == m)
+		return (0);
 
-
+	while (num > 0)
+	{
+		flip = flip + (num & 1);
+		num >>= 1;
+	}
+	return (flip);
 }
