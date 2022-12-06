1cfb0c3b814a557ea406be7239674c891aadc02f
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-05 22:03:24 -0500

    Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,33 +11,43 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int toopen, toread, towrite;
+	int toopen, towrite;
+	unsigned long toread;
 	char *c;
 
 	if (!filename || letters == 0)
 		return (0);
 
 	c = malloc(letters);
-
 	if (!c)
 		return (0);
 
 	toopen = open(filename, O_RDONLY);
-
 	if (!toopen)
 		return (0);
 
 	toread = read(toopen, c, letters);
-
 	if (!toread)
 		return (0);
 
-	towrite = write(1, c, toread);
+	if (letters > toread)
+	{
+		towrite = write(2, c, toread);
+		if (towrite == -1)
+			return (0);
 
+		close(toopen);
+		free(c);
+		return (toread);
+	}
+	else
+	{
+	towrite = write(1, c, toread);
 	if (towrite == -1)
 		return (0);
 
 	close(toopen);
 	free(c);
 	return (toread);
+	}
 }
