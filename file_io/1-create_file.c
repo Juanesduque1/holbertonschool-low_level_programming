d8dfb074e59c65d13a885f0047d36eb95eb69fb1
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-05 22:14:37 -0500

    Task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*create_file - Create a file
+**@filename: Name of the file
+*@letters: Content to write inside the file
+*
+*Return: Always 0
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+
+	if (!filename)
+		return (-1);
+	
+}
