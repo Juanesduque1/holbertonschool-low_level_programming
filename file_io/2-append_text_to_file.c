f301891a351fca159607116aa631d3309c437948
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-08 12:31:46 -0500

    Task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,6 +15,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	fd = fopen(filename, "a");
 	if (!fd)
 		return (-1);
