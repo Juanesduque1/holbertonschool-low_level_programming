4385856f6107f0dd8b14ba3512dd6c6b928f8897
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-07 22:24:40 -0500

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,8 +10,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int text_len, to_open, to_write = 0;
+
+	text_len = strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	
+	to_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (to_open == -1)
+		return (-1);
+	
+	write(to_write, text_content, text_len);
+	close(to_open);
+	
+	return (1);
 }
