8bc3f4efb4ade337059a45e0a0f935fb68efca92
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-08 21:09:12 -0500

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = value_dup;
 	node->next = NULL;
 
-	if (ht->array[index] && ht->array[index]->key == key)
+	if (ht->array[index] && ht->array[index]->key == key_dup)
 		node->next = ht->array[index];
 
 	ht->array[index] = node;
