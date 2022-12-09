0630c8f4545b49649d6284dac35e4017fef6a2c8
Juanesduque1 <juanesduquemorales@gmail.com>
2022-12-09 00:31:46 -0500

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current->next;
 	new_node->prev = current;
+	current->next->prev = new_node;
 	current->next = new_node;
 
 	return (new_node);
